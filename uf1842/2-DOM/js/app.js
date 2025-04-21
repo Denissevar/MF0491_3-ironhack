7fc8a187e375cd43573d88eebfe51bbb16c85d37
Denissevar <dvargasvel@gmail.com>
2025-04-22 00:34:03 +0200

    DOM mensaje

--- uf1842/2-DOM/js/app.js
+++ uf1842/2-DOM/js/app.js
@@ -1,3 +1,20 @@
 /**
  * ¡Buena suerte! ¡Pasa el mensaje!
  */
+const input = document.querySelector(`#message`);
+const button = document.querySelector(`#btn-send`);
+const output = document.querySelector(`#last-message`);
+const feedback = document.querySelector(`.feedback`);
+
+button.addEventListener(`click`, function () {
+    const value = input.value.trim();
+
+    if (value === ``) {
+        feedback.classList.add(`show`);
+        output.textContent = `Ningun mensaje enviado todavia`;
+    } else {
+        output.textContent = value;
+        input.value = '';
+        feedback.classList.remove(`show`);
+    }
+});
